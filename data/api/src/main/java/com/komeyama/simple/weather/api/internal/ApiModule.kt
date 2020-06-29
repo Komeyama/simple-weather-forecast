b9047110e0d3f0ff2a5d993b8309abd13415b866
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-06-29 22:06:50 +0900

    add HttpClient provider and fix gradle

--- data/api/src/main/java/com/komeyama/simple/weather/api/internal/ApiModule.kt
+++ data/api/src/main/java/com/komeyama/simple/weather/api/internal/ApiModule.kt
@@ -4,6 +4,12 @@ import com.google.gson.GsonBuilder
 import com.komeyama.simple.weather.api.ForecastApi
 import dagger.Module
 import dagger.Provides
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -32,5 +38,14 @@ internal class ApiModule {
     @Singleton
     fun provideForecastApi(retrofit: Retrofit): ForecastApi {
         return retrofit.create(ForecastApi::class.java)
+    }
+
+    @Provides
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(OkHttp) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer()
+            }
+        }
     }
 }
