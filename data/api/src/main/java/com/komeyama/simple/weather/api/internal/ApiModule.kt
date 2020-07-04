f859ad79c3d5e1145866f45403092f04d165fb7a
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-07-04 17:09:08 +0900

    refactor api module

--- data/api/src/main/java/com/komeyama/simple/weather/api/internal/ApiModule.kt
+++ data/api/src/main/java/com/komeyama/simple/weather/api/internal/ApiModule.kt
@@ -1,6 +1,5 @@
 package com.komeyama.simple.weather.api.internal
 
-import com.google.gson.GsonBuilder
 import com.komeyama.simple.weather.api.ForecastApi
 import dagger.Module
 import dagger.Provides
@@ -10,30 +9,10 @@ import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 internal class ApiModule {
-
-    companion object {
-        const val WEATHER_URL = "http://weather.livedoor.com/"
-    }
-
-    @Provides
-    @Singleton
-    fun retrofit(): Retrofit {
-        return Retrofit.Builder()
-            .addConverterFactory(
-                GsonConverterFactory.create(
-                    GsonBuilder().serializeNulls().create()
-                )
-            )
-            .baseUrl(WEATHER_URL)
-            .build()
-    }
-
 
     @Provides
     fun provideHttpClient(): HttpClient {
