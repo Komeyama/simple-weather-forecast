1167236877e39451081b296dba34a4717ce9fde8
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-07-18 15:54:42 +0900

    fix import

--- feature/weather/src/main/java/com/komeyama/simple/weather/weather_list/di/WeatherDetailListAssistedInjectModule.kt
+++ feature/weather/src/main/java/com/komeyama/simple/weather/weather_list/di/WeatherDetailListAssistedInjectModule.kt
@@ -1,6 +1,5 @@
 package com.komeyama.simple.weather.weather_list.di
 
-import com.komeyama.simple.weather.weather_list.viewmodel.AssistedInject_WeatherDetailListAssistedInjectModule
 import com.squareup.inject.assisted.dagger2.AssistedModule
 import dagger.Module
 
