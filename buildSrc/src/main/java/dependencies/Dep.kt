32a86f00523071b1fc7de7d3dd37d56558f0af6a
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-07-19 13:09:09 +0900

    add: buildsrc to make the library versions consistent

---
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -0,0 +1,14 @@
+package dependencies
+
+object Dep {
+    object Dagger {
+        val version = "2.25.4"
+        val core = "com.google.dagger:dagger:$version"
+        val compiler = "com.google.dagger:dagger-compiler:$version"
+        val androidSupport = "com.google.dagger:dagger-android-support:$version"
+        val android = "com.google.dagger:dagger-android:$version"
+        val androidProcessor = "com.google.dagger:dagger-android-processor:$version"
+        val assistedInjectAnnotations = "com.squareup.inject:assisted-inject-annotations-dagger2:0.5.2"
+        val assistedInjectProcessor = "com.squareup.inject:assisted-inject-processor-dagger2:0.5.2"
+    }
+}
