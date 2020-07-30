7678d765ae4603e4159d04a627f7406923c272ef
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-07-31 01:07:45 +0900

    add: recycle view list on search place view

--- feature/weather/src/main/java/com/komeyama/simple/weather/weather_list/SearchPlacesFragment.kt
+++ feature/weather/src/main/java/com/komeyama/simple/weather/weather_list/SearchPlacesFragment.kt
@@ -6,9 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import com.komeyama.simple.weather.weather_list.databinding.ItemHeadderBinding
+import com.komeyama.simple.weather.weather_list.databinding.ItemPlaceNameBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Section
+import com.xwray.groupie.databinding.BindableItem
+import com.xwray.groupie.databinding.ViewHolder
 import dagger.Module
 import dagger.Provides
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.search_place.*
 
 class SearchPlacesFragment : DaggerFragment() {
 
@@ -22,6 +29,39 @@ class SearchPlacesFragment : DaggerFragment() {
             container,
             false
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val groupAdapter = GroupAdapter<ViewHolder<*>>()
+        search_place_recycler_view.adapter = groupAdapter
+        val section = Section()
+        section.setHeader(HeaderItem())
+        section.update(
+            listOf(
+                ForecastContentItem("test1"),
+                ForecastContentItem("test2"),
+                ForecastContentItem("test3")
+            )
+        )
+        groupAdapter.add(section)
+    }
+
+    internal data class ForecastContentItem(
+        private val text: String
+    ) : BindableItem<ItemPlaceNameBinding>() {
+        override fun getLayout() = R.layout.item_place_name
+
+        override fun bind(viewBinding: ItemPlaceNameBinding, position: Int) {
+            viewBinding.cityName.text = text
+        }
+    }
+
+    internal class HeaderItem() : BindableItem<ItemHeadderBinding>() {
+        override fun getLayout() = R.layout.item_headder
+
+        override fun bind(viewBinding: ItemHeadderBinding, position: Int) {}
     }
 }
 
