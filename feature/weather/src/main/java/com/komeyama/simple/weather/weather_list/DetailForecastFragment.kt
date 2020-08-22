13cd2227334d2d3ca144460392b2b0778d8b77c2
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-08-22 18:28:02 +0900

    update: detail forecast xml

--- feature/weather/src/main/java/com/komeyama/simple/weather/weather_list/DetailForecastFragment.kt
+++ feature/weather/src/main/java/com/komeyama/simple/weather/weather_list/DetailForecastFragment.kt
@@ -8,12 +8,15 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import coil.api.load
 import com.komeyama.simple.weather.core.di.PageScope
 import com.komeyama.simple.weather.core.extentions.assistedViewModels
+import com.komeyama.simple.weather.model.toFromKelvinToCelsius
 import com.komeyama.simple.weather.weather_list.viewmodel.DetailForecastViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.detail_forecast.*
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -44,7 +47,19 @@ class DetailForecastFragment : DaggerFragment() {
         detailForecastViewModel.detailForecastInfoLiveData.observe(viewLifecycleOwner,
             Observer {
                 Timber.d("detail forecast data %s", it)
+                detail_today_weather_image.load(it.list[0].weather[0].icon?.toIconUrl())
+                detail_today_weather_main.text = it.list[0].weather[0].main
+                detail_today_temp.text = it.list[0].main.temp.toFromKelvinToCelsius().toInt().toString()
+                detail_today_temp_max.text = it.list[0].main.temp_max.toFromKelvinToCelsius().toInt().toString()
+                detail_today_temp_min.text = it.list[0].main.temp_min.toFromKelvinToCelsius().toInt().toString()
             })
+    }
+
+    /**
+     * TODO: fix emergency
+     */
+    internal fun String.toIconUrl(): String {
+        return "http://openweathermap.org/img/wn/$this.png"
     }
 }
 
